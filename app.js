b1e17c3acb91af6c1117ccab0b9d3c0f103f37d3
Dexmark Tech <dexmarkdexmark@gmail.com>
2026-03-14 12:58:02 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+{
+  "name": "akaza-md-bot",
+  "description": "Multi-device WhatsApp bot",
+  "repository": "https://github.com/yourusername/Akaza-md",
+  "buildpacks": [
+    {
+      "url": "heroku/nodejs"
+    }
+  ],
+  "env": {
+    "NODE_ENV": "production"
+  }
+}
